fc20405203ab5afdd64b0f3eb99704d7405589b8
Vishak N <vishaknatarajan.n@gmail.com>
2025-07-17 13:09:44 +0530

    incremental analysis queries added

--- analyses/incremental_load/fact_ratings_load.sql
+++ analyses/incremental_load/fact_ratings_load.sql
@@ -11,6 +11,5 @@ where rating_timestamp = '2015-03-31 23:11:28.000 -0700';
 select * from fact_ratings 
 where rating_timestamp = '2015-03-30 23:40:02.000 -0700';
 
-
 SELECT MAX(rating_timestamp) FROM fact_ratings;
 
